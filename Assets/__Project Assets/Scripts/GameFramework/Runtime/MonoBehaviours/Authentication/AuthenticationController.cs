8355c6cd78aa807a3946149f5c529ea5b05281e3
d4160 <d4160@outlook.com>
2020-04-25 18:39:45 -0500

    first commit

---
+++ Assets/__Project Assets/Scripts/GameFramework/Runtime/MonoBehaviours/Authentication/AuthenticationController.cs
@@ -0,0 +1,98 @@
+﻿using UltEvents;
+using UnityEngine.UI;
+
+namespace GameFramework
+{
+    using System;
+    using UnityEngine;
+    using TMPro;
+
+    public class AuthenticationController : MonoBehaviour
+    {
+        [SerializeField] protected TextMeshProUGUI _usernameInfoText;
+        [SerializeField] protected TextMeshProUGUI _usernameInfoTextMultiplayer;
+        [SerializeField] protected TMP_InputField _loginInput;
+        [SerializeField] protected Button _loginButton;
+        [SerializeField] protected bool _authenticateAtStart;
+        [SerializeField] protected bool _connectToChatWhenAuthenticated;
+
+        // TODO Made changes
+        [SerializeField] protected UltEvent _onAllLoginComplete;
+
+        private void Start()
+        {
+            if (_authenticateAtStart)
+            {
+                if (DataManager.Instance.GameData.Authenticator.CanAuthenticate)
+                    LoginAll();
+                else
+                    SetUsernameUI();
+            }
+            else
+            {
+                SetUsernameUI();
+            }
+        }
+
+        private void SetUsernameUI()
+        {
+            if (_usernameInfoText)
+                _usernameInfoText.text = $"Logged as {DataManager.Instance.GameData.Authenticator.Username}";
+
+            if (_usernameInfoTextMultiplayer)
+                _usernameInfoTextMultiplayer.text = DataManager.Instance.GameData.Authenticator.Username;
+        }
+
+        public void LoginAll()
+        {
+            LoginAll(() =>
+            {
+                SetUsernameUI();
+
+                if (_connectToChatWhenAuthenticated)
+                    NetworkingManager.Instance.Chat.Connect();
+            });
+        }
+
+        public void LoginAll(Action onCompleted, Action onFailed = null)
+        {
+            if (_loginInput)
+            {
+                if (string.IsNullOrEmpty(_loginInput.text))
+                {
+                    NotificationPrefabsManager.Instance.InstancedMain?.Notify("Nombre está vacío...", 2f);
+                    return;
+                }
+                
+                if (_loginInput.text.Length < 2)
+                {
+                    NotificationPrefabsManager.Instance.InstancedMain?.Notify("Tu nombre es demasiado corto...",
+                        2f);
+                    return;
+                }
+            }
+
+            _loginInput.interactable = false;
+            _loginButton.interactable = false;
+
+            // Since all share the same authenticator
+            DataManager.Instance.GameData.Authenticator.Login(() =>
+            {
+                onCompleted?.Invoke();
+                _onAllLoginComplete?.Invoke();
+            }, onFailed);
+        }
+
+        public void LogoutAll()
+        {
+            // Since all share the same authenticator
+            DataManager.Instance.GameData.Authenticator.Logout();
+        }
+
+        public void SetOverrideAuthentication(bool setValue)
+        {
+            // Since all share the same authenticator
+            DataManager.Instance.GameData.Authenticator.AllowOverrideAuthentication = setValue;
+        }
+    }
+}
---
+++ Assets/__Project Assets/Scripts/Main/Runtime/MonoBehaviours/Entities/DartBoard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DartBoard : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.attachedRigidbody) return;
+        
+        other.attachedRigidbody.collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
+        other.attachedRigidbody.isKinematic = true;
+    }
+}
